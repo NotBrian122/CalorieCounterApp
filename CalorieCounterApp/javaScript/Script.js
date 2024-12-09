1a7ad031f2fd8d6340ab82c12d882e4a8722cade
Michael Gallagher <158192983+NotBrian122@users.noreply.github.com>
2024-12-09 21:47:50 +0000

    Add files via upload

--- CalorieCounterApp/javaScript/Script.js
+++ CalorieCounterApp/javaScript/Script.js
@@ -1,9 +1,51 @@
+//entering peramaeters
 
 
+document.getElementById('EnterButton').onclick = function checksBeforeSendingToAPI(){
+    let loop = true;
+    while(loop = true){
 
-fetch("https://world.openfoodfacts.net",)
-.then(response  =>console.log(response))
-.then(data => console.log(data))
-.catch(error => console.error(error));
+        let changeInputText = document.getElementById('userInputSection');
+        changeInputText.textContent ='You didnt a food to search<br>Enter one:';
+        changeInputText.style.color = 'red';
 
+        const dishItem =document.getElementById('userInput').value;
+
+        if(dishItem === null){
+            changeInputText.textContent ='You didnt a food to search<br>Enter one:';
+            changeInputText.style.color = 'red';
+        }else{
+            loop = false; 
+        }   
+    }
+
+    const restrictiveDiets = document.getElementById('restrivtiveDietsCheckBoxes').querySelectorAll('input[type="checkbox"]');
+    const selectedRestrictions = [];
+    
+    for(let i = 0; i< restrictiveDiets.lenght;i++){
+        if(restrictiveDiets[i].checked){
+            selectedRestrictions.push(restrictiveDiets[i].value);
+        }   
+    }
+}
+
+//this is the async function here, it works
+async function recepieApi(){
+const url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=side%20salad&diet=vegetarian&intolerances=gluten&includeIngredients=cheese%2Cnuts&excludeIngredients=eggs&instructionsRequired=true&fillIngredients=true&addRecipeInformation=true&addRecipeInstructions=false&addRecipeNutrition=false&maxReadyTime=45&sort=max-used-ingredients&offset=0&number=10&ranking=10';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'ecb0d06655msha906d7ba7c6fb4dp1cf144jsn93f5e5fdcf27',
+		'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+}
 
